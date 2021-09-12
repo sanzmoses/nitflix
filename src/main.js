ab54e47d35f116c3e2084aa649282b06d5a9ac3f
sanzmoses <sanzmoses@gmail.com>
2021-09-12 15:01:33 +0800

    adjust files

---
+++ src/main.js
@@ -0,0 +1,5 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+// src/main.js
+
+createApp(App).mount('#app')
---
+++ vue.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    css: {
+      loaderOptions: {
+        // pass Less.js Options to less-loader
+        less:{
+            additionalData: `@import "~@/assets/styles/global.less";`,
+        }
+      }
+    }
+  }
